4cc61a16fc82e30b011bacd098e9d64c2757401c
Haduong26 <haduongchieu@gmail.com>
2023-01-04 16:17:50 -0500

    header

--- docs/src/components/navigator/index.jsx
+++ docs/src/components/navigator/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './index.css'
 import { navRoutes } from '../../router'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser, faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 import { NavLink } from 'react-router-dom'
+import colors from '../color'
 
 function NavBar() {
     return (
         <nav className='navbar'>
             <div className='nav-logo-container'>
-                <NavLink to="/" className="nav-logo">
-                    Logo
-                </NavLink>
+                <NavLink to="/" className="nav-logo"> </NavLink>
             </div>
             {
                 navRoutes.map((item, index) => {
@@ -17,8 +18,7 @@ function NavBar() {
                         <div key={index} className="nav-div">
                             <NavLink
                                 to={item.path}
-                                className="nav-item"
-                            >
+                                className="nav-item" style={{color : colors.darkBlue}}>
                                 {item.name}
                             </NavLink>
                         </div>
@@ -26,17 +26,13 @@ function NavBar() {
                 })
             }
             <div className='nav-icon-container'>
-                <NavLink
-                    to='/login'
-                    className="nav-icon"
-                >
-                    Login Icon
+                <NavLink to='/login' className="nav-icon">
+                    <FontAwesomeIcon className='icon' icon={faCircleUser} 
+                                     style={{color : colors.darkBlue}}/>     
                 </NavLink>
-                <NavLink
-                    to='/shopping-cart'
-                    className="nav-icon"
-                >
-                    Shopping Cart Icon
+                <NavLink to='/shopping-cart' className="nav-icon">
+                    <FontAwesomeIcon className='icon' icon={faShoppingCart} 
+                                     style={{color : colors.darkBlue}}/ >     
                 </NavLink>
             </div>
         </nav>

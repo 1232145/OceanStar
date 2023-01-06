ef6b843f3472c9af1c2e0a79c32e1cd08ee937ff
Haduong26 <86004016+Haduong26@users.noreply.github.com>
2023-01-06 17:53:30 -0500

    Update index.jsx

--- docs/src/pages/contact/index.jsx
+++ docs/src/pages/contact/index.jsx
@@ -1,11 +1,1 @@
-import Footer from "../../components/footer";
-import Header from "../../components/header"
 
-const Contact = () => {
-    return (
-        <div>
-        </div>
-    )
-}
-
-export default Contact;
